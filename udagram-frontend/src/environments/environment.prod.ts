547ae0e4eafdf20dbc88a34fe38bf754c3066ef2
dodang <dodv810@gmail.com>
2022-10-17 16:09:20 +0700

    update code and screen shots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7f917368f6bf4d378e8fca991d87722-1572103760.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
